e8b24973cfc97e326ab1c008ab75e1036bc39f93
mikok42 <mikolaj@linczewski.com>
2021-04-04 17:07:15 +0200

    collections

--- NumberWizardGraphics/ViewController.swift
+++ NumberWizardGraphics/ViewController.swift
@@ -8,8 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
 
-    var wizard = NumberWizard.init(min_: 0, max_: 1000)
+    @IBOutlet var labelCollection: [UILabel]!
+    @IBOutlet var buttonOutlet: [UIButton]!
+    
+
+    
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var guessField: UILabel!
+    
+    func makeDissappear(){
+        labelCollection.forEach{element in
+            element.alpha = 0
+            
+        }
+        
+        buttonOutlet.forEach{element in
+            element.alpha = 0
+        }
+    }
+    
+    var wizard = NumberWizard(min_: 0, max_: 1000)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,22 +49,12 @@ class ViewController: UIViewController {
         guessField.text = String(wizard.nextGuess())
     }
     @IBAction func correctButton(_ sender: UIButton) {
-        isYourNLabel.alpha = 0
-        guessField.alpha = 0
         winLabel.alpha = 1
         
-        correctButtonOutlet.alpha = 0
-        lowerButtonOutlet.alpha = 0
-        higherButtonOutlet.alpha = 0
+        makeDissappear()
     }
     
-    @IBOutlet weak var correctButtonOutlet: UIButton!
-    @IBOutlet weak var lowerButtonOutlet: UIButton!
-    @IBOutlet weak var higherButtonOutlet: UIButton!
     
-    @IBOutlet weak var winLabel: UILabel!
-    @IBOutlet weak var guessField: UILabel!
-    @IBOutlet weak var isYourNLabel: UILabel!
     
 }
 
